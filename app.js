2e9808d5257a98f4bf5af3f645b25e74d21b23c5
doni.ac <132898784+Doniyor6444@users.noreply.github.com>
2025-02-24 14:07:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// PDF.js worker'ini sozlash
+
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
 // PDF fayllari va ularning tillari
